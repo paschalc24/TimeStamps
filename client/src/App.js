54dc26f6be61d63208464b858079dbf447bc08af
paschalc24 <pcorrigan@umass.edu>
2022-10-23 16:07:43 -0400

    Created navbar and container.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Header from './UIComponents/header.js'
+import Row from 'react-bootstrap/Row';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+    <Container fluid>
+      <Row><Header/></Row>
+    </Container>
     </div>
   );
 }
