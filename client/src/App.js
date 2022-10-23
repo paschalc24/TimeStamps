ba82d1b2b90e7c9c4a4ea0880744106b3df0a076
paschalc24 <pcorrigan@umass.edu>
2022-10-23 16:29:40 -0400

    Added form and corresponding style sheet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Container from 'react-bootstrap/Container';
-import Header from './UIComponents/header.js'
+import Header from './UIComponents/header.js';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import logForm from './UIComponents/form.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,6 +11,10 @@ function App() {
     <div className="App">
     <Container fluid>
       <Row><Header/></Row>
+      <Row className="mainRow">
+      <Col><logForm/></Col>
+      <Col>Col 2</Col>
+      </Row>
     </Container>
     </div>
   );
