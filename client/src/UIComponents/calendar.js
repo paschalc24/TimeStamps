537d3acb23db5e4fcde860a0720030cae9224132
paschalc24 <pcorrigan@umass.edu>
2022-10-23 23:32:48 -0400

    Disabled calendar loading values for time being

--- client/src/UIComponents/calendar.js
+++ client/src/UIComponents/calendar.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Calendar from 'react-calendar';
 import '../CSSComponents/calendar.css';
 
-function Cal({value, setValue}) {
+function Cal() {
     // const [value, setValue] = useState(new Date());
+    /*
     function onChange(nextValue) {
         setValue(nextValue);
     }
+    */
 
     return (
         <Calendar 
         className = "cal"
+        /*
         onChange={onChange}
         value={value}
+        */
         />
     );
 }
